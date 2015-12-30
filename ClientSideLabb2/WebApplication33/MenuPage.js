56d25d96618899bf25195300b7f0d5d10d708f89
ekwall <ekwallandreas@gmail.com>
2015-12-30 14:24:27 +0100

    completed first of labb2

--- ClientSideLabb2/WebApplication33/MenuPage.js
+++ ClientSideLabb2/WebApplication33/MenuPage.js
@@ -1,15 +1,75 @@
 ﻿//happy hour logic:
-
+var today;
 //Method calls:
-
+getTodaysDay();
+removeInnerHTMLToElementsByTagName("span");
+addPicturesToSpan();
+adjustPrice();
+getTodaysDay();
+displayTodaysOffer();
 
 //Adjust price function: (add .toFixed(2) for 2 decimals)
+function adjustPrice() {
+    var priceElements = document.getElementsByClassName("price");
+    var rawNumber;
+   
+    for (var i = 0; i < priceElements.length; i++) {
+
+        rawNumber = Number(priceElements[i].innerHTML);
+        priceElements[i].innerHTML = rawNumber.toFixed(2);
+    }
+}
 
 /*Add pictures function to span:*/
+function removeInnerHTMLToElementsByTagName(tagName) {
+    var element = document.getElementsByTagName(tagName);
+    for (var i = 0; i < element.length; i++) {
+        element[i].innerHTML = "";
+    }
+    
+}
 
+function addPicturesToSpan() {
+    var spanElement = document.getElementsByTagName("span");
+    
+    for (var i = 0; i < spanElement.length; i++) {
+        var newImgElement = document.createElement("img");
+        newImgElement.setAttribute("src", "/Images/Hamburger.png")
+        spanElement[i].appendChild(newImgElement);
+    }
+   
+}
 /*Add todays offer function. 
 Make price background red. 
 Reduce price by another 20%:*/
+
+function getTodaysDay() {
+    var d = new Date();
+    var todaysDay = d.getDay()-1;
+
+    if(todaysDay===-1){
+        today = 6
+    }
+    else {
+        today = todaysDay;
+    }
+    
+}
+
+function displayTodaysOffer() {
+    var fieldsetElements = document.getElementsByTagName("fieldset");
+    var todaysOfferParentFieldset = fieldsetElements[today];
+    
+    var todaysOfferDivs = todaysOfferParentFieldset.getElementsByTagName("div");
+    var todaysOfferDiv = todaysOfferDivs[0];
+    var todaysOfferPrice = Number(todaysOfferDiv.innerHTML);
+
+    todaysOfferDiv.style.backgroundColor = "red";
+    todaysOfferDiv.innerHTML = todaysOfferPrice * 0.80;
+    
+ }
+
+
 
 /*Stretch exercises: (increasing tuffness for each number!)
 
